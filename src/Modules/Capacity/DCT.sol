73d9aed9c3fd7305d4a0a8cf37bbfa5fc145e102
nftchance <chance@utc24.io>
2022-11-05 21:10:57 -0500

    feat: initial capacity curves commit

---
+++ src/Modules/Capacity/DCT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.16;
+
+import "@prb/math/contracts/PRBMathSD59x18.sol";
+
+import { Trigonometry } from "./Trigonometry.sol";
+
+contract DCT { 
+    // TODO: Implement DCT
+}
