d229d577d46953a43eb95e42516c37da1ee88768
Mason <33527785+masonchain@users.noreply.github.com>
2022-12-14 10:56:56 -0600

    npm package update

---
+++ out/Deployments/Deployments.ts
@@ -0,0 +1,51 @@
+import PaymentTokenABI from "../ERC20.sol/ERC20.json";
+import AnyReputationTokenABI from "../ERC1155.sol/ERC1155.json";
+import LaborMarketABI from "../LaborMarket.sol/LaborMarket.json";
+import ReputationEngineABI from "../ReputationEngine.sol/ReputationEngine.json";
+import LaborMarketFactoryABI from "../LaborMarketFactory.sol/LaborMarketFactory.json";
+import LaborMarketNetworkABI from "../LaborMarketNetwork.sol/LaborMarketNetwork.json";
+import ReputationModuleABI from "../ReputationModule.sol/ReputationModule.json";
+import LikertEnforcementABI from "../EnforcementCriteria.sol/EnforcementCriteria.json";
+import PaymentModuleABI from "../PaymentModule.sol/PaymentModule.json";
+import PayCurveABI from "../PayCurve.sol/PayCurve.json";
+
+export const PaymentToken = {
+    "address": "0xc932d7b29390bdbd94057457392731fb7b1f856c",
+    "abi": PaymentTokenABI
+}
+export const AnyReputationToken = {
+    "address": "0xf219b405c46406ed7786a9ec51767432a3991c79",
+    "abi": AnyReputationTokenABI
+}
+export const LaborMarket = {
+    "address": "0xccbc48a243ef99f975617b0900547e32eea7f87a",
+    "abi": LaborMarketABI
+}
+export const ReputationEngine = {
+    "address": "0x42c06b7f401d8d5ae65301881ce4331de2168729",
+    "abi": ReputationEngineABI
+}
+export const LaborMarketFactory = {
+    "address": "0x509b65a8616b6898aa9af5e4cefd1703515758b9",
+    "abi": LaborMarketFactoryABI
+}
+export const LaborMarketNetwork = {
+    "address": "0x78d8d24c5a2d8717d64ba3bedb80c2500200fcbc",
+    "abi": LaborMarketNetworkABI
+}
+export const ReputationModule = {
+    "address": "0x5f701ce3f83402398832fe163c40c5380466c099",
+    "abi": ReputationModuleABI
+}
+export const LikertEnforcement = {
+    "address": "0xb6d253d25a0019d90cf67478acf73126c3cea41a",
+    "abi": LikertEnforcementABI
+}
+export const PaymentModule = {
+    "address": "0x59ddc8a7429cda1e02cc05057da9a77f7fb9a171",
+    "abi": PaymentModuleABI
+}
+export const PayCurve = {
+    "address": "0x3055c2f474c5da064e15f09740302d1a9f5819b6",
+    "abi":  PayCurveABI
+}
